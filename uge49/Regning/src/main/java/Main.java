6b60d6ce5610d3b0b491e727443e38dcc6ead8e6
abdimo101 <abdi.abdulle@outlook.dk>
2020-12-03 15:06:26 +0100

    Add files via upload

---
+++ uge49/Regning/src/main/java/Main.java
@@ -0,0 +1,8 @@
+//træningsopgaver regning
+public class Main {
+    public static void main(String[] args) {
+        MainController mc = new MainController();
+        mc.regning(50, 20);
+
+    }
+}
---
+++ uge49/TrainingTasks/src/main/java/Main.java
@@ -0,0 +1,10 @@
+// Opgaven: Menuvalg
+public class Main {
+
+    public static void main(String[] args) {
+        MainController mc = new MainController();
+        mc.runProgram();
+
+
+    }
+}
